e0ae0ba34005ed0024140ccfb027e889213ef256
Lucas Schatz <lucasschatz@gmail.com>
2022-07-29 18:15:12 -0300

    Adicionado função para pegar relação de boletos
    `function TBancoInter.GetRelacaoBoletos(pfiltro:TFiltroBoleto): TList<TBoleto>;`

--- src/ApiInter.Model.JsonSerializable.pas
+++ src/ApiInter.Model.JsonSerializable.pas
@@ -3,27 +3,49 @@ unit ApiInter.Model.JsonSerializable;
 interface
 
 uses
-  Rest.Json, JSON;
+  Rest.Json, JSON, IdURI;
 
 type
   TJsonSerializable = class
   public
-    function ToJson: string; Virtual;
-    procedure FromJson(json:String);virtual;
+    function ToJson: TJSONObject; overload; Virtual;
+    function toURLQueryParam:String;
+    procedure FromJson(json:String);overload;virtual;
+    procedure FromJson(json:TJSONObject);overload;virtual;
   end;
 
 implementation
 
+uses
+  System.SysUtils;
 { TJsonSerializable }
 
 procedure TJsonSerializable.FromJson(json: String);
 begin
-  TJson.JsonToObject(self, TJSONObject.ParseJSONValue(json) as TJSONObject)
+  self.FromJson(TJSONObject.ParseJSONValue(json) as TJSONObject);
 end;
 
-function TJsonSerializable.ToJson: string;
+procedure TJsonSerializable.FromJson(json: TJSONObject);
 begin
-  Result := TJson.ObjectToJsonString( Self );
+  TJson.JsonToObject(self, json)
+end;
+
+function TJsonSerializable.ToJson: TJSONObject;
+begin
+  Result := TJson.ObjectToJsonObject( Self );
+end;
+
+function TJsonSerializable.toURLQueryParam: String;
+var lJSON:TJSONObject;
+  I:Integer;
+begin
+  lJSON := self.ToJson;
+  for i := 0 to lJSON.Count-1 do
+  begin
+    if i>0 then
+      result := result+'&';
+    result := result+format('%s=%s', [lJSON.Pairs[i].JsonString.Value,TIdURI.ParamsEncode(lJSON.Pairs[i].JsonValue.Value)])
+  end;
 end;
 
 end.
