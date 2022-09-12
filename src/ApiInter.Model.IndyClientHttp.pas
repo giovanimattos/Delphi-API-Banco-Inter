e267e6607fa65256e3685819ed0fde20df940c1e
arvanus <lucasschatz@gmail.com>
2022-09-11 21:11:36 -0300

    Update ApiInter.Model.IndyClientHttp.pas
    Enable handle HTTP redirects

--- src/ApiInter.Model.IndyClientHttp.pas
+++ src/ApiInter.Model.IndyClientHttp.pas
@@ -48,6 +48,7 @@ begin
   ATarget := TComponent.Create(nil);
 
   IdHTTP := TIdHTTP.Create(ATarget);
+  IdHTTP.HandleRedirects := true;
   IdHTTP.Request.BasicAuthentication := False;
   IdHTTP.Request.UserAgent := 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0';
   //IdHTTP.Request.Accept := 'application/json';
